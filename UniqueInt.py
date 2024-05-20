6fdebe9a9c59ebfad9fd39705398d7eb643dd6d5
lemwaizz <mwaidennis26@gmail.com>
2024-05-20 16:22:13 +0200

    defining static methods

--- UniqueInt.py
+++ UniqueInt.py
@@ -23,4 +23,25 @@ class UniqueInt:
             for i in range(len(seen_integers)):
                 if seen_integers[i]:
                     output_file.write(str(i - 1023) + '\n')
+    
+    @staticmethod
+    def readNextItemFromFile(line):
+        if line.strip() == '':  
+            """skipping empty lines"""
+            return None
+        
+        spaces = line.strip().split()  
+        if len(spaces) != 1:  
+            """skip lines with more than one space to account for the one integer condition"""
+            return None
+        
+        try:
+            integer = int(parts[0])  
+            if -1023 <= integer <= 1023:  
+                return integer
+        except ValueError:
+            pass
+        
+        return None #caught by the try and except block to handle cases without a valid output
+
 
