7d8c415e284bb4e83adf4a1695c41228a02d606a
lemwaizz <mwaidennis26@gmail.com>
2024-05-20 15:53:55 +0200

    start of solution

---
+++ UniqueInt.py
@@ -0,0 +1,8 @@
+"""importing required libraries"""
+import resource
+import time
+
+"""declaring file paths"""
+inputFilePath = "sample_input_for_students-20240520T133354Z-001\sample_01.txt"
+outputFilePath = "results_for_sample_inputs-20240520T133355Z-001\sample_01.txt_result.txt"
+
