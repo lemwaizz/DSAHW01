a34dfc43048eadd59d4069dd3378d149127b0a9a
lemwaizz <mwaidennis26@gmail.com>
2024-05-20 15:58:46 +0200

    remove file paths to reuse

--- UniqueInt.py
+++ UniqueInt.py
@@ -2,10 +2,6 @@
 import resource
 import time
 
-"""declaring file paths"""
-inputFilePath = "sample_input_for_students-20240520T133354Z-001\sample_01.txt"
-outputFilePath = "results_for_sample_inputs-20240520T133355Z-001\sample_01.txt_result.txt"
-
 class UniqueInt:
     """initializing class uniqueint"""
     @staticmethod
@@ -13,5 +9,4 @@ class UniqueInt:
         start_time = time.time()  # Start time for runtime tracking
         
         seen_integers = [False] * 2048 #it is initialized as a range
-        
-          
+
