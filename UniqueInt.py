877b5a1c0305b3f260c2651d6c424695cf3736e8
lemwaizz <mwaidennis26@gmail.com>
2024-05-20 15:56:20 +0200

    boolean array specifics are defined

--- UniqueInt.py
+++ UniqueInt.py
@@ -6,3 +6,12 @@ import time
 inputFilePath = "sample_input_for_students-20240520T133354Z-001\sample_01.txt"
 outputFilePath = "results_for_sample_inputs-20240520T133355Z-001\sample_01.txt_result.txt"
 
+class UniqueInt:
+    """initializing class uniqueint"""
+    @staticmethod
+    def processFile(inputFilePath, outputFilePath):
+        start_time = time.time()  # Start time for runtime tracking
+        
+        seen_integers = [False] * 2048 #it is initialized as a range
+        
+          
